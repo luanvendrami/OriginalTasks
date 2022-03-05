2b0699f6f4014e184f5fdc7878e062c7f98c1431
Luan Vendrami <luan.vendrami@hotmail.com>
2022-03-05 01:22:26 -0300

    Adicionado functions para abrir e fechar o modal ao click no botão, alterado parametros a serem enviado pelas props, enviando a propriedade como um interio,não a suas propriedades

--- src/components/Rows/index.tsx
+++ src/components/Rows/index.tsx
@@ -1,26 +1,44 @@
 import { Button } from "components/Button";
+import { NewModal } from "components/Modal";
 import { ITask } from "interfaces";
+import { useState } from "react";
 import { Badge, Container } from "./styled";
 
-type row = ITask;
+type row = {
+  dados: ITask
+};
 
-export function Row({ id, title, description, finished, created_at }: row) {
+export function Row({ dados }: row) {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewModal(){
+  setIsNewTransactionModalOpen(true);
+  }
+  
+  function handleCloseNewModal(){
+  setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <Container key={id}>
-      <td>{title}</td>
-      <td>{description}</td>
-      <td>{new Intl.DateTimeFormat("pt-BR").format(new Date(created_at))}</td>
+    <Container key={dados.id}>
+      <td>{dados.title}</td>
+      <td>{dados.description}</td>  
+      <td>{new Intl.DateTimeFormat("pt-BR").format(new Date(dados.created_at))}</td>
       <td>
-        <Badge className={finished ? "buttonBadgeTrue" : "buttonBadgeFalse"}>
-        <span>{finished ? "Finalizado" : "Pendente"}</span>
+        <Badge className={dados.finished ? "buttonBadgeTrue" : "buttonBadgeFalse"}>
+        <span>{dados.finished ? "Finalizado" : "Pendente"}</span>
         </Badge>
       </td>
       <td width={400}>
-        <Button className="button1" buttonName="Primeiro" />
-        <Button className="button2" buttonName="Segundo" />
+        <Button className="button1" buttonName="Primeiro" onClick={handleOpenNewModal} />
+        <NewModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewModal} dados={dados} value={1}/>
+        <Button className="button2" buttonName="Segundo"  onClick={handleOpenNewModal}  />
+        <NewModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewModal} dados={dados} value={2}/>
         <Button className="button3" buttonName="Terceiro" />
         <Button className="button4" buttonName="Quarto" />
       </td>
+      
     </Container>
   );
 }
