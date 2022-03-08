dda6f98f25bd3c50eeae015b3fd34076a740bd01
Luan Vendrami <luan.vendrami@hotmail.com>
2022-03-07 21:12:35 -0300

    Adicionado um Value a props, 3 condicionais em if, cada uma com um modal diferente para cada botão

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,7 +8,7 @@ interface NewTransactionModalprops {
   isOpen: boolean;
   onRequestClose: () => void;
   dados?: ITask;
-  value: number;
+  value?: number;
 }
 export function NewModal({
   isOpen,
@@ -16,12 +16,70 @@ export function NewModal({
   dados,
   value,
 }: NewTransactionModalprops) {
-
+  if(value === 1){
+    return (
+      <Modal
+        isOpen={isOpen}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        ariaHideApp={false}
+      >
+        <button
+          type="button"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+          <img src={fechar} alt="Fechar Modal" />
+        </button>
+        <Container>
+          <h2>Título</h2>
+          <Textarea
+            className="textAreaTitle"
+            value={dados?.title}
+            disabled={true}
+          />
+          <br />
+          <h2>Descrição</h2>
+          <Textarea
+            className="textAreaBody"
+            value={dados?.description}
+            disabled={true}
+          />
+        </Container>
+      </Modal>
+    );
+  }
+  if(value === 2){
+    return (
+      <Modal
+        isOpen={isOpen}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+        ariaHideApp={false}
+      >
+        <button
+          type="button"
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+          <img src={fechar} alt="Fechar Modal" />
+        </button>
+        <Container>
+          <h2>Título</h2>
+          <h1>Teste</h1>
+          <br />
+          <h2>Descrição</h2>
+          <h1>Teste</h1>
+        </Container>
+      </Modal>
+    );
+  }
   return (
     <Modal
       isOpen={isOpen}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
+      ariaHideApp={false}
     >
       <button
         type="button"
@@ -31,19 +89,7 @@ export function NewModal({
         <img src={fechar} alt="Fechar Modal" />
       </button>
       <Container>
-        <h2>Título</h2>
-        <Textarea
-          className="textAreaTitle"
-          value={dados?.title}
-          disabled={true}
-        />
-        <br />
-        <h2>Descrição</h2>
-        <Textarea
-          className="textAreaBody"
-          value={dados?.description}
-          disabled={true}
-        />
+        <h2>vazrio</h2>
       </Container>
     </Modal>
   );
