f4b77d7acc92e93125652797b4fb1e89a011019a
Luan Vendrami <luan.vendrami@hotmail.com>
2022-03-07 21:13:30 -0300

    retirado html e adicionado prosp children, passando para a Page home seu html base e funcionalidades

--- src/components/Rows/index.tsx
+++ src/components/Rows/index.tsx
@@ -1,44 +1,14 @@
 import { Button } from "components/Button";
 import { NewModal } from "components/Modal";
 import { ITask } from "interfaces";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { Badge, Container } from "./styled";
 
 type row = {
-  dados: ITask
+  dados: ITask;
+  children: ReactNode;
 };
 
-export function Row({ dados }: row) {
-
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-  function handleOpenNewModal(){
-  setIsNewTransactionModalOpen(true);
-  }
-  
-  function handleCloseNewModal(){
-  setIsNewTransactionModalOpen(false);
-  }
-
-  return (
-    <Container key={dados.id}>
-      <td>{dados.title}</td>
-      <td>{dados.description}</td>  
-      <td>{new Intl.DateTimeFormat("pt-BR").format(new Date(dados.created_at))}</td>
-      <td>
-        <Badge className={dados.finished ? "buttonBadgeTrue" : "buttonBadgeFalse"}>
-        <span>{dados.finished ? "Finalizado" : "Pendente"}</span>
-        </Badge>
-      </td>
-      <td width={400}>
-        <Button className="button1" buttonName="Primeiro" onClick={handleOpenNewModal} />
-        <NewModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewModal} dados={dados} value={1}/>
-        <Button className="button2" buttonName="Segundo"  onClick={handleOpenNewModal}  />
-        <NewModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewModal} dados={dados} value={2}/>
-        <Button className="button3" buttonName="Terceiro" />
-        <Button className="button4" buttonName="Quarto" />
-      </td>
-      
-    </Container>
-  );
+export function Row({ dados, children }: row) {
+  return <Container key={dados.id}>{children}</Container>;
 }
